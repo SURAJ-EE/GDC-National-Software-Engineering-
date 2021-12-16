422e9c2c0ba17884becb0622cb6500e432583d3c
SURAJ-EE <84455536+SURAJ-EE@users.noreply.github.com>
2021-12-16 20:14:09 +0530

    Add files via upload

---
+++ oops.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+class student{
+    public:
+string name;
+int age;
+bool gender;
+};
+
+
+int main(){
+student a;
+a.name = 'Suraj';
+a.age = 20;
+a.gender = 0;       
+    return 0;
+}
